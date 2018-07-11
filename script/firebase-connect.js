6562031a6b67336eb0edebef686e832e807801d9
Rijin Mk <rijin.mk9@gmail.com>
2018-07-12 00:42:03 +0400

    Update firebase-connect.js

--- script/firebase-connect.js
+++ script/firebase-connect.js
@@ -1,10 +1,10 @@
 // Initialize Firebase
 var config = {
-  apiKey: "AIzaSyCKWJqS7CFo616G1pV5IgFQd8vwkRlQnQU",
+  apiKey: "<API KEY>",
   authDomain: "manipal-hallticket.firebaseapp.com",
   databaseURL: "https://manipal-hallticket.firebaseio.com",
   projectId: "manipal-hallticket",
   storageBucket: "manipal-hallticket.appspot.com",
-  messagingSenderId: "675413850672"
+  messagingSenderId: "<MESSAGING SENDER ID>"
 };
 firebase.initializeApp(config);
